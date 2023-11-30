ea59276cd9ae8d6ed686815558b2d6847399e896
Threather <144106347+Threather@users.noreply.github.com>
2023-11-30 14:32:34 +0700

    Modify and Added more Change on JS

--- script.js
+++ script.js
@@ -1,38 +1,36 @@
-// Get the 'About me' header
-var aboutMeHeader = document.querySelector('#aboutMe');
-var experienceHeader = document.querySelector('#experience');
-var skillHeader = document.querySelector('#skill'); 
+// Get the headers
+var headers = {
+    'aboutMe': document.querySelector('#aboutMe'),
+    'experience': document.querySelector('#experience'),
+    'skill': document.querySelector('#skill')
+};
 
-// Get the 'studentInfo' div
-var studentInfo = document.querySelector('#studentInfo');
-var experienceInfo = document.querySelector('#experienceInfo');
-var skillInfo = document.querySelector('#skillInfo'); // Adjusted this line
+// Get the sections
+var sections = {
+    'aboutMe': document.querySelector('#studentInfo'),
+    'experience': document.querySelector('#experienceInfo'),
+    'skill': document.querySelector('#skillInfo')
+};
 
-// Initially hide the 'studentInfo', 'experienceInfo', and 'Skill' divs
-studentInfo.style.display = 'none';
-experienceInfo.style.display = 'none';
-skillInfo.style.display = 'none'; // Add this line
+// Initially hide all sections
+for (let section in sections) {
+    sections[section].style.display = 'none';
+}
 
-// Add a click event listener to the 'About me' header
-aboutMeHeader.addEventListener('click', function() {
-    // Show the 'studentInfo' div and hide the other divs
-    studentInfo.style.display = 'block';
-    experienceInfo.style.display = 'none';
-    skillInfo.style.display = 'none'; // Add this line
-});
+// Function to hide all sections except the one to show
+function hideAllExcept(showSectionKey) {
+    for (let section in sections) {
+        if (section === showSectionKey) {
+            sections[section].style.display = sections[section].style.display === 'block' ? 'none' : 'block';
+        } else {
+            sections[section].style.display = 'none';
+        }
+    }
+}
 
-// Add a click event listener to the 'Experience' header
-experienceHeader.addEventListener('click', function() {
-    // Show the 'experienceInfo' div and hide the other divs
-    experienceInfo.style.display = 'block';
-    studentInfo.style.display = 'none';
-    skillInfo.style.display = 'none'; // Add this line
-});
-
-// Add a click event listener to the 'Skill' header
-skillHeader.addEventListener('click', function() { 
-    // Show the 'Skill' div and hide the other divs
-    skillInfo.style.display = 'block';
-    studentInfo.style.display = 'none';
-    experienceInfo.style.display = 'none';
-});
+// Add event listeners to each header
+for (let header in headers) {
+    headers[header].addEventListener('click', function() {
+        hideAllExcept(header);
+    });
+}
